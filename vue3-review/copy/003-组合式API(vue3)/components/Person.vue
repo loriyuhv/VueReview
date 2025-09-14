71f488c17d43cff892ef8ae80b631ef89a02251e
loriyuhv <2385923602@qq.com>
2025-09-14 10:47:30 +0800

    feat: 003-组合式API(vue3)

---
+++ vue3-review/copy/003-组合式API(vue3)/components/Person.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="person">
+    <h2>个人信息</h2>
+    <p>姓名: {{ name }}</p>
+    <p>年龄: {{ age }}</p>
+    <button @click="changeName">修改名字</button>
+    <button @click="changeAge">修改年龄</button>
+    <button @click="showTel">查看联系方式</button>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "Person", // 组件名称
+  setup() {
+    /* 数据 */
+    let name = "John Doe"; /* 非响应式数据 */
+    let age = 30; /* 非响应式数据 */
+    let tel = "123-456-7890";
+    /* 方法 */
+    function changeName() {
+      name = "Jane Smith";
+    }
+    function changeAge() {
+      age += 2;
+    }
+    function showTel() {
+      alert("联系方式: " + tel);  
+    }
+    // 返回需要在模板中使用的数据和方法
+    return {name, age, changeName, changeAge, showTel};
+  }
+}
+</script>
+
+<style>
+  .person {
+    background-color: skyblue;
+    box-shadow: 0 0 10px;
+    border-radius: 10px;
+    padding: 20px;
+  }
+</style>
--- vue3-review/src/components/Person.vue
+++ vue3-review/src/components/Person.vue
@@ -5,7 +5,6 @@
     <p>年龄: {{ age }}</p>
     <button @click="changeName">修改名字</button>
     <button @click="changeAge">修改年龄</button>
-    <button @click="age++">年龄+1</button>
     <button @click="showTel">查看联系方式</button>
   </div>
 </template>
@@ -13,23 +12,23 @@
 <script lang="ts">
 export default {
   name: "Person", // 组件名称
-  // 组件数据
-  data() {
-    return {
-      name: "John Doe",
-      age: 30
-    };
-  },
-  methods: {
-    changeName() {
-      this.name = "Jane Smith";
-    },
-    changeAge() {
-      this.age += 2;
-    },
-    showTel() {
-      alert("联系方式: 123-456-7890");  
+  setup() {
+    /* 数据 */
+    let name = "John Doe"; /* 非响应式数据 */
+    let age = 30; /* 非响应式数据 */
+    let tel = "123-456-7890";
+    /* 方法 */
+    function changeName() {
+      name = "Jane Smith";
     }
+    function changeAge() {
+      age += 2;
+    }
+    function showTel() {
+      alert("联系方式: " + tel);  
+    }
+    // 返回需要在模板中使用的数据和方法
+    return {name, age, changeName, changeAge, showTel};
   }
 }
 </script>
