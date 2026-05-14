d4db8568d5648499acf5c0751ee0beaf4174ae1e
loriyuhv <2385923602@qq.com>
2026-05-14 08:06:36 +0800

    feat(vue3-study)：001-写一个App组件

---
+++ vue3-study/src/App.vue
@@ -0,0 +1,22 @@
+<script lang="ts">
+// 暴露组件选项
+export default {
+  // 组件名称
+  name: 'App',
+}
+</script>
+
+<template>
+  <div class="app">
+    <h1>Hello vue3</h1>
+  </div>
+</template>
+
+<style scoped>
+.app {
+  background-color: skyblue;
+  box-shadow: 0 0 10px;
+  border-radius: 10px;
+  padding: 20px;
+}
+</style>
