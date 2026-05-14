61896cbe54793a9e8b976f79345f04eae3fa79f8
loriyuhv <2385923602@qq.com>
2026-05-14 14:29:58 +0800

    feat(vue3-study): 008-reactive创建_对象类型的响应式数据

---
+++ vue3-study/copy/008-reactive创建_对象类型的响应式数据/App.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+import PersonVue from './components/PersonVue.vue'
+
+/* 暴露组件选项 */
+export default {
+  /* 组件名称 */
+  name: 'App',
+  components: {
+    /* 注册Person组件 */
+    PersonVue,
+  },
+}
+</script>
+
+<template>
+  <PersonVue />
+</template>
+
+<style scoped></style>
