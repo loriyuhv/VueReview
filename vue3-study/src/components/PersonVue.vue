8b8beb5579ee132a1d7f8bc0dd5e19d40396cf86
loriyuhv <2385923602@qq.com>
2026-05-14 09:34:17 +0800

    feat(vue3-study)：006-setup语法糖

---
+++ vue3-study/src/components/PersonVue.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="person">
+    <h2>个人信息</h2>
+    <p>姓名: {{ name }}</p>
+    <p>年龄: {{ age }}</p>
+    <button @click="changeName">修改名字</button>
+    <button @click="changeAge">修改年龄</button>
+    <button @click="showTel">查看联系方式</button>
+  </div>
+</template>
+
+<!--
+  引入vite-plugin-vue-setup-extend插件可以省略以下代码
+  1. 安装：npm i vite-plugin-vue-setup-extend -D
+  2. 在vite.config.ts文件中引入
+  import vueSetupExtend from 'vite-plugin-vue-setup-extend'
+  plugins: [
+    vueSetupExtend(), // setup语法糖所需插件
+  ],
+-->
+
+<!-- <script lang="ts">
+export default {
+  name: 'PersonVue', // 组件名称
+}
+</script> -->
+
+<script setup lang="ts">
+/* 数据 */
+let name = 'Jerry'
+let age = 20
+const tel = '123-456-7890'
+
+/* 方法 */
+function changeName() {
+  name = 'Jane Smith'
+}
+function changeAge() {
+  age += 2
+}
+function showTel() {
+  alert('联系方式: ' + tel)
+}
+</script>
+
+<style scoped>
+.person {
+  background-color: skyblue;
+  box-shadow: 0 0 10px;
+  border-radius: 10px;
+  padding: 20px;
+}
+
+button {
+  margin-right: 10px;
+}
+</style>
