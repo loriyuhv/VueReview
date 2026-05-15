a06474380d7eb6c5f8ff85f216c0d4aac305283b
loriyuhv <2385923602@qq.com>
2026-05-15 14:07:08 +0800

    feat(vue3-study): 014-watchEffect使用

---
+++ vue3-study/copy/014-watchEffect使用/App.vue
@@ -0,0 +1,23 @@
+<script lang="ts">
+import PersonVue from './components/PersonVue.vue'
+
+/* 暴露组件选项 */
+export default {
+  /* 组件名称 */
+  name: 'App',
+  components: {
+    /* 注册Person组件 */
+    PersonVue,
+  },
+}
+</script>
+
+<template>
+  <PersonVue />
+</template>
+
+<style scoped>
+div {
+  font-family: 'JetBrainsMonoNL Nerd Font Mono', 'Noto Sans CJK SC', monospace;
+}
+</style>
