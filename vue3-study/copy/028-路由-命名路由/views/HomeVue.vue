f59c29cbe211bd13921514b5b653b4a8e6d2eb63
loriyuhv <2385923602@qq.com>
2026-08-07 06:01:39 +0800

    feat(vue3-study):028-路由-命名路由

---
+++ vue3-study/copy/028-路由-命名路由/views/HomeVue.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="home">
+    <img src="http://www.atguigu.com/images/index_new/logo.png" alt="" />
+  </div>
+</template>
+
+<style scoped>
+.home {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+}
+</style>
