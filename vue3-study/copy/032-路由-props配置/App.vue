6fda02bfaa61d9b9b37476c0b4be6b23d761e4cd
loriyuhv <2385923602@qq.com>
2026-08-07 07:15:02 +0800

    feat(vue3-study):032-路由-props配置

---
+++ vue3-study/copy/032-路由-props配置/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="app">
+    <HeaderVue/>
+    <!--  导航区  -->
+    <div class="navigate">
+      <RouterLink to="/home" active-class="active">首页</RouterLink>
+      <RouterLink :to="{name: 'information'}" active-class="active">新闻</RouterLink>
+      <RouterLink :to="{path: '/about'}" active-class="active">关于</RouterLink>
+    </div>
+    <!-- 展示区 -->
+    <div class="main-content">
+      <RouterView />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import HeaderVue from '@/components/HeaderVue.vue'
+import { RouterView, RouterLink } from 'vue-router'
+</script>
+
+<style>
+.navigate {
+  display: flex;
+  justify-content: space-around;
+  margin: 0 100px;
+}
+.navigate a {
+  display: block;
+  text-align: center;
+  width: 90px;
+  height: 40px;
+  line-height: 40px;
+  border-radius: 10px;
+  background-color: gray;
+  text-decoration: none;
+  color: white;
+  font-size: 18px;
+  letter-spacing: 5px;
+}
+.navigate a.active {
+  background-color: #64967e;
+  color: #ffc268;
+  font-weight: 900;
+  text-shadow: 0 0 1px black;
+  font-family: Sarasa UI SC,serif;
+}
+.main-content {
+  margin: 30px auto 0;
+  border-radius: 10px;
+  width: 90%;
+  height: 400px;
+  border: 1px solid;
+}
+</style>
