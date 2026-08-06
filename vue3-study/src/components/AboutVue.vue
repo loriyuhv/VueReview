e18a6b1958b16e0baf9f15996a23d0c3e2917bf0
loriyuhv <2385923602@qq.com>
2026-08-07 05:44:57 +0800

    feat(vue3-study):024-路由-基本切换效果

---
+++ vue3-study/src/components/AboutVue.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="about">
+    <h2>大家好，欢迎来到尚硅谷直播间</h2>
+  </div>
+</template>
+
+<style scoped>
+.about {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  color: rgb(85, 84, 84);
+  font-size: 18px;
+}
+</style>
---
+++ vue3-study/src/router/index.ts
@@ -0,0 +1,35 @@
+// 创建一个路由器，并暴露出去
+
+// 第一步：引入createRouter
+import {createRouter, createWebHistory} from 'vue-router'
+
+// 引入组件
+import HomeVue from '../components/HomeVue.vue'
+import NewsVue from '../components/NewsVue.vue'
+import AboutVue from '../components/AboutVue.vue'
+
+// 第二步：创建路由器
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      component: HomeVue,
+    },
+    {
+      path: '/home',
+      component: HomeVue,
+    },
+    {
+      path: '/news',
+      component: NewsVue,
+    },
+    {
+      path: '/about',
+      component: AboutVue,
+    },
+  ],
+})
+
+// 暴露出去router
+export default router
